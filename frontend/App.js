6806aeaa7995c66a3d72a4907d41f4bf2b903208
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-09-08 13:27:20 +0200

    Backend & Frontend socket connection setup

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { io } from "socket.io-client"
 
 export default function App() {
+  const socket = io("http://localhost:8000/");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
