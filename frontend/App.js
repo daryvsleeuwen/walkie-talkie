a714fd4b486e7e09b85b64bdeaafe53eb994e8b6
NielsdeVreede <nielsdevreede01@gmail.com>
2021-09-14 13:37:21 +0200

    fixed frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,51 +1,74 @@
-import {StatusBar} from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
-import * as Font from 'expo-font';
-import {io} from 'socket.io-client';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button,PermissionsAndroid , StyleSheet, Text, View } from 'react-native';
+
+import LiveAudioStream from 'react-native-live-audio-stream';
 
 export default function App() {
-  let [fontsLoaded, setFontsLoaded] = useState(false);
-  const socket = io('http://localhost:8000/');
 
-  const loadFonts = async () => {
-    await Font.loadAsync({
-      PoppinsThin: require('./assets/fonts/Poppins-Thin.ttf'),
-      PoppinsRegular: require('./assets/fonts/Poppins-Regular.ttf'),
-      PoppinsMedium: require('./assets/fonts/Poppins-Medium.ttf'),
-      PoppinsSemiBold: require('./assets/fonts/Poppins-Bold.ttf'),
-      PoppinsBold: require('./assets/fonts/Poppins-SemiBold.ttf'),
-    });
-
-    setFontsLoaded(true);
+  let record = () => {
+    console.log("recording...")
+    const options = {
+    sampleRate: 32000,  // default is 44100 but 32000 is adequate for accurate voice recognition
+    channels: 1,        // 1 or 2, default 1
+    bitsPerSample: 16,  // 8 or 16, default 16
+    audioSource: 6,     // android only (see below)
+    bufferSize: 4096    // default is 2048
   };
-
-
-  useEffect(() => {
-    loadFonts();
+  
+  LiveAudioStream.init(options);
+  LiveAudioStream.on('data', data => {
+    console.log(data);
+    // base64-encoded audio data chunks
   });
-
-  if (fontsLoaded) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.pageTitle}>pageTitle</Text>
-      </View>
-    );
-  } else {
-    return null;
+  LiveAudioStream.start();
   }
+
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.jsss to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="record"
+        onPress={record}></Button>
+      <Button title="request permissions" onPress={requestMicrophonePermission} />
+    </View>
+  );
+}
+
+const requestMicrophonePermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: "Microphone usage",
+          message:
+           "Please accept for microphone usage",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      console.log(granted)
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Permission granted")
+      } else {
+        console.log("Microphone permission denied");
+      }
+  } catch (err) {
+    console.warn(err);
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 80,
-    paddingBottom: 80,
-    paddingLeft: 20,
-    paddingRight: 20,
-  },
-
-  pageTitle: {
-    fontFamily: 'PoppinsBold',
-    fontSize: 32,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
