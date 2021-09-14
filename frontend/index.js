681a06e9eabb3974048500d646a2be10f2b22da3
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-09-14 20:25:55 +0200

    Root component now rendered without expo

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,4 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-
+import {AppRegistry} from 'react-native';
 import App from './App';
 
-
-// registerRootComponent calls AprpRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+AppRegistry.registerComponent('main', () => App);
