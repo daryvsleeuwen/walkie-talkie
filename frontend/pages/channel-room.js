d6ea8d2fc391ab00024125a6d947fcce4a252742
daryvsleeuwen <daryvansleeuwen@hotmail.nl>
2021-09-22 14:24:11 +0200

    Updating all joined users when new user joines

--- frontend/pages/channel-room.js
+++ frontend/pages/channel-room.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect} from 'react';
 import LiveAudioStream from 'react-native-live-audio-stream';
 import BackNavigation from '../components/back-navigator';
 import {Animated, PermissionsAndroid, StyleSheet, Text, View, Pressable, Easing} from 'react-native';
@@ -10,8 +11,6 @@ import 'react-native-url-polyfill/auto';
 import JoinedUser from '../components/joined-user';
 import styles from '../styles/misc';
 
-let socket;
-let roomid = 0;
 const options = {
   sampleRate: 16000,
   channels: 1,
@@ -20,85 +19,14 @@ const options = {
   bufferSize: 4096,
 };
 
+let socket = (socket = io('http://145.93.141.68:8000'));
+let counter = 0;
+let base64String = '';
+let chunknum = 0;
+let base64data = '';
+let joined = false;
+
 LiveAudioStream.init(options);
-
-LiveAudioStream.on('data', (data) => {
-  socket.emit('audio', {
-    roomid: roomid,
-    audiodata: data,
-  });
-});
-
-let leaveRoom = () => {
-  LiveAudioStream.stop();
-  socket.emit('leave_room', roomid);
-};
-
-let connect = () => {
-  SoundPlayer.addEventListener('FinishedLoadingURL', ({success, url}) => {
-    console.log('finished loading url', success, url);
-  });
-
-  socket = io('http://145.93.117.164:8000');
-
-  socket.on('connect', () => {
-    socket.emit('join_room', roomid);
-    //SoundPlayer.playUrl('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3');
-  });
-
-  let counter = 0;
-  let base64String = '';
-  let chunknum = 0;
-  let base64data = '';
-
-  socket.on('data_incoming', (data) => {
-    let chunk = Buffer.from(data, 'base64');
-    let blob = new Blob([chunk], {type: 'audio/wav'});
-
-    const url = 'data:audio/wav;base64,' + data; //URL.createObjectURL(blob.blob);
-    const fileReaderInstance = new FileReader();
-
-    fileReaderInstance.readAsDataURL(blob);
-
-    fileReaderInstance.onload = () => {
-      base64data = fileReaderInstance.result;
-    };
-    try {
-      console.log(base64data);
-      SoundPlayer.playUrl(base64data);
-    } catch (e) {}
-  });
-  //   if (counter < 0){
-  //     console.log(counter);
-  //     base64String = base64String + data;
-  //     counter++;
-  //   }  else{
-  //     console.log('creating wav file');
-  //     counter = 0;
-  //     const path = RNFS.ExternalDirectoryPath + "/chunk_num" + chunknum + '.wav'
-  //     RNFS.writeFile(path, data, "base64")
-  //     .then((success) => {
-  //       console.log('wrote to file')
-  //       console.log(success)
-  //     })
-  //     .catch((err) => {
-  //       console.log(err)
-  //     });
-  //     chunknum++;
-
-  //     SoundPlayer.playSoundFile(path.slice(0, path.length -4), 'wav')
-  //   }
-  // })
-};
-
-let send = () => {
-  console.log('sending data');
-
-  // socket.emit('audio', {
-  //   roomid: roomid,
-  //   audiodata: 'data',
-  // });
-};
 
 const requestMicrophonePermission = async () => {
   try {
@@ -116,15 +44,55 @@ const requestMicrophonePermission = async () => {
 
 export default function ChannelRoom(props) {
   const {navigate} = props.navigation;
-  const {frequency} = props.route.params;
+  let {roomid, frequency} = props.route.params;
+  let [joinedUsers, setJoinedUsers] = React.useState([]);
   const animatedButtonScale = new Animated.Value(1);
-  connect();
+  let mounted = false;
+
+  useEffect(() => {
+    mounted = true;
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  if (!joined) {
+    socket.emit('join_room', roomid);
+    joined = true;
+
+    socket.on('update_joined_users', (joinedUsers) => {
+      if(mounted){
+        setJoinedUsers(joinedUsers.updated);
+      }
+    });
+
+    LiveAudioStream.on('data', (data) => {
+      socket.emit('audio', {
+        roomid: roomid,
+        audiodata: data,
+      });
+    });
+  }
+
+  const leaveRoom = () => {
+    LiveAudioStream.stop();
+    socket.emit('leave_room', roomid);
+    joined = false;
+  };
+
+  const send = () => {
+    // socket.emit('audio', {
+    //   roomid: roomid,
+    //   audiodata: 'data',
+    // });
+  };
 
   const scaleButtonDown = () => {
     Animated.timing(animatedButtonScale, {
       toValue: 0.8,
       duration: 300,
-      easing: Easing.bezier(.38,.46,.08,.91),
+      easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
       useNativeDriver: true,
     }).start();
   };
@@ -132,7 +100,7 @@ export default function ChannelRoom(props) {
   const scaleButtonUp = () => {
     Animated.timing(animatedButtonScale, {
       toValue: 1,
-      easing: Easing.bezier(.38,.46,.08,.91),
+      easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
       duration: 300,
       useNativeDriver: true,
     }).start();
@@ -147,7 +115,11 @@ export default function ChannelRoom(props) {
 
       {/* Display all clients in channel */}
       <View style={styles.pageContent}>
-        <JoinedUser userName="talker#24523" talking={true}></JoinedUser>
+        {joinedUsers.map((user, index) => {
+          if (user !== socket.id) {
+            return <JoinedUser key={index} userName={user} talking={false}></JoinedUser>;
+          }
+        })}
       </View>
 
       <View style={pageStyles.pushToTalkCenterer}>
