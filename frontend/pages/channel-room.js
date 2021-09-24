03abfc7898050a4566cae6f19096d9f7b99b77c4
niels <n.devreede@student.fontys.nl>
2021-09-24 12:10:57 +0200

    initial setup for agora

--- frontend/pages/channel-room.js
+++ frontend/pages/channel-room.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import LiveAudioStream from 'react-native-live-audio-stream';
 import BackNavigation from '../components/back-navigator';
-import {Animated, PermissionsAndroid, StyleSheet, Text, View, Pressable, Easing} from 'react-native';
-import {io} from 'socket.io-client';
-import {Buffer} from 'buffer';
+import { Animated, PermissionsAndroid, StyleSheet, Text, View, Pressable, Easing } from 'react-native';
+import { io } from 'socket.io-client';
+import { Buffer } from 'buffer';
 import RNFS from 'react-native-fs';
 import SoundPlayer from 'react-native-sound-player';
 import 'react-native-url-polyfill/auto';
 import JoinedUser from '../components/joined-user';
 import styles from '../styles/misc';
+import RtcEngine from 'react-native-agora'
 // import Sound from 'react-native-sound';
 
 let joined = false;
@@ -37,11 +38,23 @@ const requestMicrophonePermission = async () => {
   }
 };
 
+let state = {
+  appId: `973ff918e3064ce4ba5e71bac6d06267`,
+  token: '006973ff918e3064ce4ba5e71bac6d06267IADbdLrlhAcym0Fbb3hJwyH4bmNhtk+VLfiiPL4cg2AgdwZa8+gAAAAAEACtrEr9HdtOYQEAAQAd205h',
+  channelName: 'testing',
+  openMicrophone: true,
+  enableSpeakerphone: true,
+  joinSucceed: false,
+  peerIds: [],
+}
+
+
+
 const animatedButtonScale = new Animated.Value(1);
 
 export default function ChannelRoom(props) {
-  const {navigate} = props.navigation;
-  let {roomid, frequency, socket} = props.route.params;
+  const { navigate } = props.navigation;
+  let { roomid, frequency, socket } = props.route.params;
   let [joinedUsers, setJoinedUsers] = React.useState([]);
 
   let audioData;
@@ -98,115 +111,137 @@ export default function ChannelRoom(props) {
         counter++;
       }
     });
+
+    //AGORA
+    let engine;
+    RtcEngine.create(state.appId).then(rtcEngine => {
+      engine = rtcEngine;
+
+      console.log(engine)
+      engine.enableAudio();
+
+      engine.addListener('UserJoined', (uid, elapsed) => {
+        console.log('User joined', uid, elapsed)
+      })
+
+      engine.addListener('JoinChannelSuccess', (channel, uid, elapsed) => {
+        console.log('JoinChannelSuccess', channel, uid, elapsed)
+      })
+
+      engine.joinChannel(state.token, state.channelName, null, 0);
+    })
+
+
+
+
+    const leaveRoom = () => {
+      LiveAudioStream.stop();
+      socket.emit('leave_room', roomid);
+      joined = false;
+    };
+
+    const send = () => {
+      LiveAudioStream.start();
+      socket.emit('set_talking_state', true);
+    };
+
+    const stopSending = () => {
+      LiveAudioStream.stop();
+      socket.emit('set_talking_state', false);
+    };
+
+    const scaleButtonDown = () => {
+      Animated.timing(animatedButtonScale, {
+        toValue: 0.8,
+        duration: 300,
+        easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
+        useNativeDriver: true,
+      }).start();
+    };
+
+    const scaleButtonUp = () => {
+      Animated.timing(animatedButtonScale, {
+        toValue: 1,
+        easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    };
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.pageTitleBox}>
+          <BackNavigation navigate={navigate} onBack={leaveRoom}></BackNavigation>
+          <Text style={styles.pageTitle}>Channel {frequency}</Text>
+        </View>
+
+        {/* Display all clients in channel */}
+        <View style={styles.pageContent}>
+          {joinedUsers.map((user, index) => {
+            if (user.id !== socket.id) {
+              return <JoinedUser key={index} userName={user.id} talking={user.talking}></JoinedUser>;
+            }
+          })}
+        </View>
+
+        <View style={pageStyles.pushToTalkCenterer}>
+          <Animated.View style={[{ transform: [{ scale: animatedButtonScale }], position: 'relative' }, styles.fullCenter]}>
+            <View style={pageStyles.pushToTalkBackground}></View>
+            <Pressable
+              style={pageStyles.pushToTalkbutton}
+              onPressIn={() => {
+                scaleButtonDown();
+                send();
+              }}
+              onPressOut={() => {
+                scaleButtonUp();
+                stopSending();
+              }}>
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}>
+                <Text style={pageStyles.pushToTalkText}>Push me</Text>
+              </View>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </View>
+    );
   }
 
-  const leaveRoom = () => {
-    LiveAudioStream.stop();
-    socket.emit('leave_room', roomid);
-    joined = false;
-  };
+  const pageStyles = StyleSheet.create({
+    pushToTalkCenterer: {
+      width: '100%',
+      justifyContent: 'center',
+    },
 
-  const send = () => {
-    LiveAudioStream.start();
-    socket.emit('set_talking_state', true);
-  };
+    pushToTalkBackground: {
+      width: 164,
+      height: 164,
+      borderRadius: 82,
+      backgroundColor: 'black',
+      position: 'absolute',
+    },
 
-  const stopSending = () => {
-    LiveAudioStream.stop();
-    socket.emit('set_talking_state', false);
-  };
+    pushToTalkbutton: {
+      width: 160,
+      height: 160,
+      borderRadius: 80,
+      backgroundColor: '#FF4848',
+      borderWidth: 3,
+      borderColor: 'white',
+      padding: 10,
+      alignItems: 'center',
+    },
 
-  const scaleButtonDown = () => {
-    Animated.timing(animatedButtonScale, {
-      toValue: 0.8,
-      duration: 300,
-      easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
-      useNativeDriver: true,
-    }).start();
-  };
-
-  const scaleButtonUp = () => {
-    Animated.timing(animatedButtonScale, {
-      toValue: 1,
-      easing: Easing.bezier(0.38, 0.46, 0.08, 0.91),
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
-  };
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.pageTitleBox}>
-        <BackNavigation navigate={navigate} onBack={leaveRoom}></BackNavigation>
-        <Text style={styles.pageTitle}>Channel {frequency}</Text>
-      </View>
-
-      {/* Display all clients in channel */}
-      <View style={styles.pageContent}>
-        {joinedUsers.map((user, index) => {
-          if (user.id !== socket.id) {
-            return <JoinedUser key={index} userName={user.id} talking={user.talking}></JoinedUser>;
-          }
-        })}
-      </View>
-
-      <View style={pageStyles.pushToTalkCenterer}>
-        <Animated.View style={[{transform: [{scale: animatedButtonScale}], position: 'relative'}, styles.fullCenter]}>
-          <View style={pageStyles.pushToTalkBackground}></View>
-          <Pressable
-            style={pageStyles.pushToTalkbutton}
-            onPressIn={() => {
-              scaleButtonDown();
-              send();
-            }}
-            onPressOut={() => {
-              scaleButtonUp();
-              stopSending();
-            }}>
-            <View
-              style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Text style={pageStyles.pushToTalkText}>Push me</Text>
-            </View>
-          </Pressable>
-        </Animated.View>
-      </View>
-    </View>
-  );
+    pushToTalkText: {
+      fontSize: 20,
+      fontFamily: 'Poppins-Medium',
+      color: '#ffffff',
+      textAlign: 'center',
+    },
+  });
 }
-
-const pageStyles = StyleSheet.create({
-  pushToTalkCenterer: {
-    width: '100%',
-    justifyContent: 'center',
-  },
-
-  pushToTalkBackground: {
-    width: 164,
-    height: 164,
-    borderRadius: 82,
-    backgroundColor: 'black',
-    position: 'absolute',
-  },
-
-  pushToTalkbutton: {
-    width: 160,
-    height: 160,
-    borderRadius: 80,
-    backgroundColor: '#FF4848',
-    borderWidth: 3,
-    borderColor: 'white',
-    padding: 10,
-    alignItems: 'center',
-  },
-
-  pushToTalkText: {
-    fontSize: 20,
-    fontFamily: 'Poppins-Medium',
-    color: '#ffffff',
-    textAlign: 'center',
-  },
-});
